a17da1188ab07745d682d8f9334feeb61f88927a
miraclesu <suchuangji@gmail.com>
2018-06-05 21:18:14 +0800

    realtime: fixed test

--- realtime/client_test.go
+++ realtime/client_test.go
@@ -1,6 +1,7 @@
 package realtime
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/RocketChat/Rocket.Chat.Go.SDK/common_testing"
@@ -15,10 +16,10 @@ var (
 func getLoggedInClient(t *testing.T) *Client {
 
 	if client == nil {
-		c, err := NewClient(common_testing.Host, common_testing.Port, true)
+		c, err := NewClient(&url.URL{Host: common_testing.Host + ":" + common_testing.Port}, true)
 		assert.Nil(t, err, "Couldn't create realtime client")
 
-		err = c.RegisterUser(&models.UserCredentials{
+		_, err = c.RegisterUser(&models.UserCredentials{
 			Email:    common_testing.GetRandomEmail(),
 			Name:     common_testing.GetRandomString(),
 			Password: common_testing.GetRandomString()})
