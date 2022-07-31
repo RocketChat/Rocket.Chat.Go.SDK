9844da7881ccd8987363ed193b4e638ca2bcf416
evan slack <evan.slack@tufts.edu>
2022-07-31 13:23:07 -0400

    add tests for users

--- rest/users_test.go
+++ rest/users_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/RocketChat/Rocket.Chat.Go.SDK/common_testing"
-	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -15,16 +14,26 @@ func TestRocket_LoginLogout(t *testing.T) {
 }
 
 func TestRocket_CreateUser(t *testing.T) {
-	client := getDefaultClient(t)
+	rocket := getDefaultClient(t)
 
-	newUser := &models.CreateUserRequest{
+	newUser := &createUserRequest{
 		Name:     "Test User",
 		Email:    "testUser@test.com",
 		Password: "testPassword",
 		Username: "TestUser",
 	}
 
-	createdUser, err := client.CreateUser(newUser)
+	createdUser, err := rocket.CreateUser(newUser)
 	assert.Nil(t, err)
 	assert.NotNil(t, createdUser)
+}
+
+func TestRocket_GetUserStatus(t *testing.T) {
+	rocket := getDefaultClient(t)
+
+	status, err := rocket.GetUserStatus(testUserName)
+	assert.Nil(t, err)
+	assert.NotNil(t, status)
+	assert.Equal(t, "online", status.Status)
+	assert.Equal(t, "online", status.ConnectionStatus)
 }
