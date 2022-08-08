fead41d3cff57ba1e125f1ab554128aff935f82e
srrathi <srrathi2000@gmail.com>
2022-08-08 17:27:51 +0530

    added REST function to execute a slash command

--- rest/information.go
+++ rest/information.go
@@ -1,6 +1,8 @@
 package rest
 
 import (
+	"bytes"
+	"fmt"
 	"net/url"
 
 	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
@@ -13,8 +15,11 @@ type InfoResponse struct {
 
 type SlashCommandsResponse struct {
 	Status
-	Success  bool                  `json:"success"`
 	Commands []models.SlashCommand `json:"commands"`
+}
+
+type ExecuteSlashCommandResponse struct {
+	Status
 }
 
 // GetServerInfo a simple method, requires no authentication,
@@ -114,4 +119,18 @@ func (c *Client) GetSlashCommandsList(params url.Values) ([]models.SlashCommand,
 		return nil, err
 	}
 	return response.Commands, nil
+}
+
+// ExecuteSlashCommand
+// Execute a slash command in a room in the Rocket.Chat server
+// command and roomId are required params is optional it depends upon command
+//
+// https://developer.rocket.chat/reference/api/rest-api/endpoints/core-endpoints/commands-endpoints/execute-a-slash-command
+func (c *Client) ExecuteSlashCommand(channel *models.ChannelSubscription, command string, params string) (ExecuteSlashCommandResponse, error) {
+	response := new(ExecuteSlashCommandResponse)
+	var body = fmt.Sprintf(`{ "command": "%s", "roomId": "%s", "params": "%s"}`, command, string(channel.RoomId), params)
+	if err := c.Post("commands.run", bytes.NewBufferString(body), response); err != nil {
+		return *response, err
+	}
+	return *response, nil
 }
--- rest/information_test.go
+++ rest/information_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/RocketChat/Rocket.Chat.Go.SDK/common_testing"
+	"github.com/RocketChat/Rocket.Chat.Go.SDK/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -63,4 +64,14 @@ func TestRocket_GetSlashCommandsList(t *testing.T) {
 	slashCommands, err := rocket.GetSlashCommandsList(url.Values{"offset": []string{"0"}, "count": []string{"50"}})
 	assert.Nil(t, err)
 	assert.NotNil(t, slashCommands)
+}
+
+func TestRocket_ExecuteSlashCommand(t *testing.T) {
+	rocket := Client{Protocol: common_testing.Protocol, Host: common_testing.Host, Port: common_testing.Port}
+	// TODO admin user
+	rocket.auth = &authInfo{id: "4SicoW2wDjcAaRh4M", token: "4nI24JkcHTsqTtOUeJYbbrhum5T_Y6IdjAwyj72qDBu"}
+	general := &models.ChannelSubscription{RoomId: "GENERAL"}
+	resp, err := rocket.ExecuteSlashCommand(general, "tableflip", "@all")
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
 }
