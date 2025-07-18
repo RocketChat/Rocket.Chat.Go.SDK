ec16db1c426287633db1ad81b94be67721e3217a
evan slack <51209817+evanofslack@users.noreply.github.com>
2025-07-18 01:35:45 -0400

    Add methods to expand coverage of `channels` endpoints (#55)
    * move groups methods into new file
    
    * add CreateChannel method
    
    * add UserInfo method
    
    * add InviteChannel method
    
    * add JoinChannel method
    
    * add GetChannelMemebers method
    
    * update CreateChannel method to accept parameters instead of a struct
    
    * remove unecessary comment from tests
    
    * change response types to be private
    
    ---------
    
    Co-authored-by: evan slack <evan.slack@tufts.edu>

--- rest/channels_test.go
+++ rest/channels_test.go
@@ -45,3 +45,48 @@ func TestRocket_GetChannelInfo(t *testing.T) {
 	assert.NotEmpty(t, updatedChannelInfo.UpdatedAt)
 	assert.NotEmpty(t, updatedChannelInfo.Timestamp)
 }
+
+func TestRocket_CreateChannel(t *testing.T) {
+	rocket := getDefaultClient(t)
+
+	// create channel with same name as testUserName so that channels aren't duplicated
+	_, err := rocket.CreateChannel(testUserName, nil, false)
+	assert.Nil(t, err)
+}
+
+func TestRocket_InviteChannel(t *testing.T) {
+	rocket := getDefaultClient(t)
+
+	general := &models.Channel{ID: "GENERAL"}
+	user, err := rocket.UserInfo(testUserName)
+	assert.Nil(t, err)
+
+	invitedUsers := []*models.User{user}
+	_, err = rocket.InviteChannel(general, invitedUsers)
+	assert.Nil(t, err)
+}
+
+func TestRocket_JoinChannel(t *testing.T) {
+	rocket := getDefaultClient(t)
+	general := &models.Channel{ID: "GENERAL"}
+	_, err := rocket.JoinChannel(general, "")
+	assert.Nil(t, err)
+}
+
+func TestRocket_GetChannelMembers(t *testing.T) {
+	rocket := getDefaultClient(t)
+	t.Run("With channel name", func(t *testing.T) {
+		general := &models.Channel{Name: "general"}
+		members, err := rocket.GetChannelMembers(general)
+		assert.Nil(t, err)
+		assert.GreaterOrEqual(t, len(members), 1)
+
+	})
+	t.Run("With channel ID", func(t *testing.T) {
+		general := &models.Channel{ID: "GENERAL"}
+		members, err := rocket.GetChannelMembers(general)
+		assert.Nil(t, err)
+		assert.GreaterOrEqual(t, len(members), 1)
+	})
+
+}
